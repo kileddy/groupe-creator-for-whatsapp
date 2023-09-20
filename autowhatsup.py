7855b506c48cc8364e7c19152ae2cd027c5c19a4
kileddy <135814460+kileddy@users.noreply.github.com>
2023-09-21 02:07:38 +0300

    Update autowhatsup.py

--- autowhatsup.py
+++ autowhatsup.py
@@ -5,17 +5,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Инициализация веб-драйвера (Firefox)
 driver = webdriver.Firefox()
 
-# Открытие WhatsApp Web
 driver.get('https://web.whatsapp.com/')
 input('Отсканируйте QR-код в браузере и нажмите Enter после входа в WhatsApp Web...')
 
-# Функция для создания группы
 def create_group():
     try:
-        # Открыть меню создания новой группы
+        
         new_chat_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[title="Новый чат"]')))
         new_chat_button.click()
         
@@ -27,10 +24,10 @@ def create_group():
         for contact in contacts:
             search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input.l7jjieqr')))
             search_box.send_keys(contact)
-            time.sleep(1)  # Подождать, чтобы появились результаты
-            search_box.send_keys(Keys.RETURN)  # Добавлен Enter
+            time.sleep(1)  
+            search_box.send_keys(Keys.RETURN)  
         
-        # Нажать кнопку "Далее"
+        
         next_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.d6ll3xky')))
         next_button.click()
 
@@ -39,23 +36,22 @@ def create_group():
         for admin in admins:
             admin_search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[title="Поиск или выбор участника"]')))
             admin_search_box.send_keys(admin)
-            time.sleep(1)  # Подождать, чтобы появились результаты
+            time.sleep(1)  
             admin_search_box.send_keys(Keys.RETURN)
         
-        # Дать имя группе и создать ее
-        group_name = 'TestGroupebyYwelir'
+        # Дать имя группе и создать ее (замените название на свое)
+        group_name = 'TestGroupebyKILEDDY'
         group_name_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '._1PBlB')))
         group_name_box.send_keys(group_name)
         
-        # Нажать кнопку "Создать"
         create_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.d6ll3xky')))
         create_button.click()
     
     except Exception as e:
         print(f"Произошла ошибка: {str(e)}")
 
-# Создать группу
+
 create_group()
 
-# Закрыть браузер после выполнения задачи
+
 driver.quit() 
